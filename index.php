272941db56504c17491fefd2d5f1244417f6c56d
John <johndmersereau@gmail.com>
2021-05-02 20:55:04 -0400

    hompage update

--- index.php
+++ index.php
@@ -7,8 +7,27 @@
   </head>
   <body>
     <div class="body">
-      <div class="panel"><center><h1>FEATURES</h1></center></div>
-      <div class="panel"><center><h1>ABOUT MEDMAP </br> AND </br> PREVIEWS</h1></center></div>
+      <div class="panel">
+        <center><h1>FEATURES</h1></center></br>
+        <li>
+          <i>Track medications.</i>
+        </li>
+        <li>
+          <i>Keep track of and get reminders for when you need to take medicine.</i>
+        </li>
+      </div>
+      <div class="panel">
+        <center><h1>ABOUT MEDMAP</h1></center></br>
+          <li>
+            <i>The only website you'll need for your medication tracking needs.</i>
+          </li>
+          <li>
+            <i>Track all your medications, including their name, quantity and description.</i>
+          </li>
+          <li>
+            <i>Set events in our planner to remind you when you need to take medications, get a refill, or anything else you wish.</i>
+          </li>
+      </div>
     </div>
     <div class="header"><form>
       <div id="left"><button formaction="Home.php" id="l" style="background-color: inherit;">
